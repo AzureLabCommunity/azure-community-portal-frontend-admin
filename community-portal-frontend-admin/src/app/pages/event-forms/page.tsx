aef6642b50c386d9d81c96e5984831365e2574b5
Diktator <dychervony@gmail.com>
2024-06-03 12:10:46 +0300

    Сделал 1ю версию формы. Добавил в паблик изображение для невыбранного

--- community-portal-frontend-admin/src/app/pages/event-forms/page.tsx
+++ community-portal-frontend-admin/src/app/pages/event-forms/page.tsx
@@ -4,11 +4,37 @@ export default function EventForm() {
   return (
     <>
       <form className="forms form-option">
-        <div className="for-input">
-          <span>title</span>
-          <input className="inputs" type="text"></input>
-          <span className="validation">error!</span>
-        </div>
+        <section className="left">
+          <img src="/textures/invalid_images.svg" alt="Selected"/>
+          <span className="validation"></span>
+          <label className="buttons file-button">
+            Choose file
+            <input type="file" accept="image/*" style={{ display: 'none' }}/>
+          </label>
+        </section>
+        <section className="right">
+          <div className="for-input">
+            <span>title</span>
+            <input className="inputs" type="text"/>
+            <span className="validation"></span>
+          </div>
+          <div className="for-input second">
+            <span>description</span>
+            <textarea className="inputs sec-input"/>
+            <span className="validation"></span>
+          </div>
+          <div className="for-input">
+            <span>datetime</span>
+            <input className="inputs" type="datetime-local"/>
+            <span className="validation"></span>
+          </div>
+          <div className="for-input">
+            <span>location</span>
+            <input className="inputs" type="text"/>
+            <span className="validation"></span>
+          </div>
+          <button className='buttons'>apply</button>
+        </section>
       </form>
     </>
   );
